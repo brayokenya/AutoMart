d83ca6a4d8d711408618efec24cddb0d704f67a6
Osahon Oboite <osahonoboite@gmail.com>
2019-05-09 22:59:15 +0100

    reveal ads on scroll

---
+++ UI/js/index.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('#wrapper');
+const scrollBtn = document.querySelector('#scroll-btn');
+
+
+scrollBtn.addEventListener('click', () => {
+    wrapper.style.display = 'block';
+});
