b80d8f31945fb17f1ea6d301b17f9a810625ac7c
Osahon Oboite <osahonoboite@gmail.com>
2019-05-10 02:55:47 +0100

    blur content when side-nav is open

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,7 +1,41 @@
+const topNav = document.querySelector('nav');
 const wrapper = document.querySelector('#wrapper');
 const scrollBtn = document.querySelector('#scroll-btn');
+const menuBtn = document.querySelector('#menu-btn');
+const blurLayer = document.querySelector('#blur-layer');
+const sideNav = document.querySelector('#side-nav');
+const closeBtn = document.querySelector('#close-btn');
 
 
 scrollBtn.addEventListener('click', () => {
     wrapper.style.display = 'block';
+    topNav.style.background = '#003542';
+});
+
+
+menuBtn.addEventListener('click', () => {
+    if (!blurLayer.style.display || blurLayer.style.display === 'none') {
+        blurLayer.style.display = 'block';
+        sideNav.style.width = '200px';
+        menuBtn.style.display = 'none';
+        closeBtn.style.display = 'block';
+    }
+});
+
+closeBtn.addEventListener('click', () => {
+    if (blurLayer.style.display = 'block') {
+        blurLayer.style.display = 'none';
+        sideNav.style.width = '0';
+        menuBtn.style.display = 'block';
+        closeBtn.style.display = 'none';
+    }
+});
+
+blurLayer.addEventListener('click', () => {
+    if (blurLayer.style.display = 'block') {
+        blurLayer.style.display = 'none';
+        sideNav.style.width = '0';
+        menuBtn.style.display = 'block';
+        closeBtn.style.display = 'none';
+    }
 });
