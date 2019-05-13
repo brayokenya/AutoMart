55108bad0c9067063ab4c30dbc607dd6b25330fd
Osahon Oboite <osahonoboite@gmail.com>
2019-05-13 20:16:06 +0100

    design a page that enable sellers to mark an ad as sold [delivers #165869048]

--- UI/js/updatePrice.js
+++ UI/js/updatePrice.js
@@ -2,8 +2,11 @@ const editPriceBtn = document.querySelector('#edit-price-btn');
 const price = document.querySelector('#price');
 const inputDiv = document.querySelector('#input-div');
 const newPriceInput = document.querySelector('#new-price');
-const cancelBtn = document.querySelector('#cancel-btn');
+const cancelBtns = document.querySelectorAll('.cancel-btn');
 const updateBtn = document.querySelector('#update-btn');
+const markAsSoldBtn = document.querySelector('#mark-as-sold-btn');
+const confirmationPrompt = document.querySelector('#confirmation-prompt');
+const confirmSaleBtn = document.querySelector('#confirm-sale-btn');
 
 
 editPriceBtn.addEventListener('click', () => {
@@ -17,10 +20,20 @@ updateBtn.addEventListener('click', () => {
         price.innerHTML = `N ${newPriceInput.value}`;
         blurLayer.style.display = '';
         inputDiv.style.display = '';
+        confirmationPrompt.style.display = '';
     }
 });
 
-cancelBtn.addEventListener('click', () => {
-    blurLayer.style.display = '';
-    inputDiv.style.display = '';
+cancelBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        blurLayer.style.display = '';
+        inputDiv.style.display = '';
+        confirmationPrompt.style.display = '';
+    });
+});
+
+markAsSoldBtn.addEventListener('click', () => {
+    blurLayer.style.display = 'block';
+    confirmationPrompt.style.display = 'block';
+    blurLayer.removeEventListener('click', blur);
 });
