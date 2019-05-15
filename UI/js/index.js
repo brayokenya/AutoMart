1250cb2a5849a8c19b3f97929ef93b085cdc9b26
Osahon Oboite <osahonoboite@gmail.com>
2019-05-15 08:06:55 +0100

    Hide sub navbar items. Show them when the parent nav item is clicked

--- UI/js/index.js
+++ UI/js/index.js
@@ -2,6 +2,9 @@ const menuBtn = document.querySelector('#menu-btn');
 const blurLayer = document.querySelector('#blur-layer');
 const sideNav = document.querySelector('#side-nav');
 const closeBtn = document.querySelector('#close-btn');
+const applyFilterBtn = document.querySelector('#apply-filter');
+const filterIcon = document.querySelector('#filter-icon');
+const hiddenFilters = document.querySelectorAll('.hide');
 
 menuBtn.addEventListener('click', () => {
     if (!blurLayer.style.display || blurLayer.style.display === 'none') {
@@ -30,3 +33,15 @@ const blur = () => {
     }
 }
 blurLayer.addEventListener('click', blur);
+
+
+applyFilterBtn.addEventListener('click', (e) => {
+    if (hiddenFilters[0].style.display === 'block') {
+        hiddenFilters.forEach(filter => filter.style.display = 'none');
+        filterIcon.innerHTML = `<i class="fas fa-chevron-right mg-l-10"></i>`;
+    } else {
+        hiddenFilters.forEach(filter => filter.style.display = 'block');
+        filterIcon.innerHTML = `<i class="fas fa-chevron-down mg-l-10"></i>`;
+    }
+    e.preventDefault();
+})
