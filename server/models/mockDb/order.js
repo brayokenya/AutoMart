b7a6f29d2c61d30033f5f6e6dac14dd47151e956
Osahon Oboite <osahonoboite@gmail.com>
2019-05-16 03:59:23 +0100

    add dummy data models

---
+++ server/models/mockDb/order.js
@@ -0,0 +1,84 @@
+const order = [
+    {
+        id: 1,
+        buyer: 3,
+        carId: 1,
+        amount: 800000,
+        status: 'rejected',
+        createdOn: '14-02-2019'
+    },
+    {
+        id: 2,
+        buyer: 4,
+        carId: 2,
+        amount: 2000000,
+        status: 'pending',
+        createdOn: '19-02-2019'
+    },
+    {
+        id: 3,
+        buyer: 2,
+        carId: 3,
+        amount: 3500000,
+        status: 'accepted',
+        createdOn: '20-03-2019'
+    },
+    {
+        id: 4,
+        buyer: 3,
+        carId: 4,
+        amount: 4500000,
+        status: 'pending',
+        createdOn: '20-03-2019'
+    },
+    {
+        id: 4,
+        buyer: 5,
+        carId: 5,
+        amount: 5500000,
+        status: 'pending',
+        createdOn: '20-03-2019'
+    },
+    {
+        id: 5,
+        buyer: 7,
+        carId: 6,
+        amount: 5500000,
+        status: 'rejected',
+        createdOn: '22-03-2019'
+    },
+    {
+        id: 6,
+        buyer: 4,
+        carId: 1,
+        amount: 2700000,
+        status: 'pending',
+        createdOn: '24-03-2019'
+    },
+    {
+        id: 7,
+        buyer: 5,
+        carId: 7,
+        amount: 2500000,
+        status: 'pending',
+        createdOn: '31-03-2019'
+    },
+    {
+        id: 8,
+        buyer: 2,
+        carId: 8,
+        amount: 10900000,
+        status: 'pending',
+        createdOn: '01-04-2019'
+    },
+    {
+        id: 9,
+        buyer: 3,
+        carId: 6,
+        amount: 95000000,
+        status: 'accepted',
+        createdOn: '02-04-2019'
+    }
+];
+
+export default order;
