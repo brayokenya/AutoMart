6cbf534ea4f90e9af7c37aa4670cfaf7308a75d0
Osahon Oboite <osahonoboite@gmail.com>
2019-05-19 05:47:25 +0100

    add price of cars to orders

--- server/models/mockDb/order.js
+++ server/models/mockDb/order.js
@@ -1,9 +1,10 @@
-const order = [
+const orders = [
     {
         id: 1,
         buyer: 3,
         carId: 1,
-        amount: 800000,
+        offer: 800000,
+        price: 3000000,
         status: 'rejected',
         createdOn: '14-02-2019'
     },
@@ -11,7 +12,8 @@ const order = [
         id: 2,
         buyer: 4,
         carId: 2,
-        amount: 2000000,
+        offer: 2000000,
+        price: 2500000,
         status: 'pending',
         createdOn: '19-02-2019'
     },
@@ -19,7 +21,8 @@ const order = [
         id: 3,
         buyer: 2,
         carId: 3,
-        amount: 3500000,
+        offer: 3500000,
+        price: 2500000,
         status: 'accepted',
         createdOn: '20-03-2019'
     },
@@ -27,7 +30,8 @@ const order = [
         id: 4,
         buyer: 3,
         carId: 4,
-        amount: 4500000,
+        offer: 4500000,
+        price: 3500000,
         status: 'pending',
         createdOn: '20-03-2019'
     },
@@ -35,7 +39,8 @@ const order = [
         id: 4,
         buyer: 5,
         carId: 5,
-        amount: 5500000,
+        offer: 5500000,
+        price: 8500000,
         status: 'pending',
         createdOn: '20-03-2019'
     },
@@ -43,7 +48,8 @@ const order = [
         id: 5,
         buyer: 7,
         carId: 6,
-        amount: 5500000,
+        offer: 5500000,
+        price: 9500000,
         status: 'rejected',
         createdOn: '22-03-2019'
     },
@@ -51,7 +57,8 @@ const order = [
         id: 6,
         buyer: 4,
         carId: 1,
-        amount: 2700000,
+        offer: 2700000,
+        price: 3000000,
         status: 'pending',
         createdOn: '24-03-2019'
     },
@@ -59,7 +66,8 @@ const order = [
         id: 7,
         buyer: 5,
         carId: 7,
-        amount: 2500000,
+        offer: 2500000,
+        price: 2500000,
         status: 'pending',
         createdOn: '31-03-2019'
     },
@@ -67,7 +75,8 @@ const order = [
         id: 8,
         buyer: 2,
         carId: 8,
-        amount: 10900000,
+        offer: 10900000,
+        price: 1100000,
         status: 'pending',
         createdOn: '01-04-2019'
     },
@@ -75,10 +84,11 @@ const order = [
         id: 9,
         buyer: 3,
         carId: 6,
-        amount: 95000000,
+        offer: 95000000,
+        price: 9500000,
         status: 'accepted',
         createdOn: '02-04-2019'
     }
 ];
 
-export default order;
+export default orders;
