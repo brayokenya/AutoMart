8158ac3c29860c9ea7316ded9df34b518946a693
Osahon Oboite <osahonoboite@gmail.com>
2019-05-23 03:27:07 +0100

    feat(flag): create api endpoint to report car ads [delivers #166021585]

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import userRouter from './server/routes/user';
 import carRouter from './server/routes/car';
 import orderRouter from './server/routes/order';
+import flagRouter from './server/routes/flag';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/', userRouter, carRouter, orderRouter);
+app.use('/api/v1/', userRouter, carRouter, orderRouter, flagRouter);
 
 const { PORT } = process.env;
 /* eslint-disable-next-line */
