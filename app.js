d5e60f4212389ce83774b8daa7a163bb7c5adcb4
Osahon Oboite <osahonoboite@gmail.com>
2019-05-27 17:11:13 +0100

    add error handlers

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(stringFormater);
 
 app.use('/api/v1/', userRouter, carRouter, orderRouter, flagRouter);
+app.all('/*', (req, res) => {
+    res.status(404).json({
+        status: 'error',
+        message: 'page not found'
+    });
+});
+
+/* eslint-disable-next-line */
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'oops! something went wrong'
+    });
+});
 
 const { PORT } = process.env;
 /* eslint-disable-next-line */
