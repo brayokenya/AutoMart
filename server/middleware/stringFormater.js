aed102f104fe0292f91a3bdb057a4af55b9b982b
Osahon Oboite <osahonoboite@gmail.com>
2019-05-27 16:57:31 +0100

    lint

--- server/middleware/stringFormater.js
+++ server/middleware/stringFormater.js
@@ -1,6 +1,6 @@
 const stringFormater = (req, res, next) => {
     const stringKeys = Object.keys(req.body);
-    stringKeys.forEach(key => {
+    stringKeys.forEach((key) => {
         if (typeof (req.body[key]) === 'string') {
             req.body[key] = req.body[key].trim();
         }
