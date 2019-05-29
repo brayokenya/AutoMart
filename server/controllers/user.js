9cd24057b34a760a5990f55ecd99311d36ebed36
Osahon Oboite <osahonoboite@gmail.com>
2019-05-29 22:20:06 +0100

    Remove unneccessary trim function

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,7 +13,7 @@ export const signupUser = (req, res) => {
     const existingUser = userQueries
         .findUserByEmail(req.body.email);
     if (existingUser) {
-        return errorMessage(res, 409, 'Email is already in use');
+        return errorMessage(res, 409, 'email is already in use');
     }
     const {
         firstName,
@@ -23,20 +23,18 @@ export const signupUser = (req, res) => {
         address
     } = req.body;
 
-    const trimmedValues = {
-        firstName: firstName.trim(),
-        lastName: lastName.trim(),
-        email: email.trim(),
-        address: address.trim(),
-        password: password.trim()
-    };
-
-    const newUser = userQueries.createUser(trimmedValues);
+    const newUser = userQueries.createUser({
+        firstName,
+        lastName,
+        email,
+        password,
+        address
+    });
     const token = generateToken(newUser.id, newUser.email);
 
     return res.status(201).json({
         status: 'success',
-        message: `Welcome, ${newUser.firstName}!`,
+        message: `welcome, ${newUser.firstName}!`,
         data: {
             token,
             id: newUser.id,
@@ -51,7 +49,7 @@ export const loginUser = (req, res) => {
     const { email, password } = req.body;
     const existingUser = userQueries.findUserByEmail(email.trim());
     if (!existingUser) {
-        return errorMessage(res, 404, 'Incorrect email or password');
+        return errorMessage(res, 404, 'incorrect email or password');
     }
 
     const isUser = existingUser.password === password.trim();
@@ -59,7 +57,7 @@ export const loginUser = (req, res) => {
     return isUser
         ? res.status(200).json({
             status: 'success',
-            message: `Welcome back, ${existingUser.firstName}!`,
+            message: `welcome back, ${existingUser.firstName}!`,
             data: {
                 token: generateToken(existingUser.id, existingUser.email),
                 id: existingUser.id,
@@ -68,14 +66,14 @@ export const loginUser = (req, res) => {
                 email: existingUser.email
             }
         })
-        : errorMessage(res, 404, 'Incorrect email or password');
+        : errorMessage(res, 404, 'incorrect email or password');
 };
 
 export const sendResetPasswordLink = (req, res) => {
     const user = userQueries
         .findUserByEmail(req.body.email.trim());
 
-    if (!user) return errorMessage(res, 404, 'User account not found');
+    if (!user) return errorMessage(res, 404, 'user account not found');
     const token = generateToken(user.id, user.email);
     const resetLink = `${process.env.APP_URL}/auth/reset-password/${token}`;
 
@@ -83,7 +81,7 @@ export const sendResetPasswordLink = (req, res) => {
     return mailgun.messages().send(messageData, () => (
         res.status(200).json({
             status: 'success',
-            message: 'A password-reset link has been sent to your email'
+            message: 'a password-reset link has been sent to your email'
         })
     ));
 };
@@ -91,10 +89,10 @@ export const sendResetPasswordLink = (req, res) => {
 export const resetPassword = (req, res) => {
     const { password } = req.body;
     const { id: userId } = getUserFromToken(req.params.token);
-    if (!userId) return errorMessage(res, 404, 'User not found. Reset link may have expired');
+    if (!userId) return errorMessage(res, 404, 'user not found. reset link may have expired');
     userQueries.updatePassword(userId, password);
     return res.status(200).json({
         status: 'success',
-        message: 'Password was successfully updated'
+        message: 'password was successfully updated'
     });
 };
