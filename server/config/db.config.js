420677e465bff36fa388a46ff1b6ab46fbd5f23d
Osahon Oboite <osahonoboite@gmail.com>
2019-06-17 06:01:10 +0100

    configure postgresql

---
+++ server/config/db.config.js
@@ -0,0 +1,9 @@
+import { Pool } from 'pg';
+import appVariables from './app.config';
+
+const connectionString = appVariables.dbUrl;
+
+const pool = new Pool({ connectionString });
+
+
+export default pool;
