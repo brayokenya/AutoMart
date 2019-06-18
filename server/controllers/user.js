ef6c743766f9513b73b88da829363ecbfaa674f5
Osahon Oboite <osahonoboite@gmail.com>
2019-06-18 03:38:27 +0100

    feat(user): create api endpoint for logging in users [delivers #166718194]

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,7 +5,7 @@ import userQueries from '../models/db/queries';
 import { generateToken } from '../middleware/jwtAuth';
 import errorMessage from '../helpers/responseMessages';
 
-const signupUser = async (req, res) => {
+export const signupUser = async (req, res) => {
     const {
         firstName,
         lastName,
@@ -37,4 +37,31 @@ const signupUser = async (req, res) => {
     }
 };
 
-export default signupUser;
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await userQueries.findUserByEmail(email);
+
+        if (!existingUser) return errorMessage(res, 404, 'incorrect email or password');
+
+        const passwordsMatch = await bcrypt.compare(password, existingUser.password_hash);
+        if (!passwordsMatch) return errorMessage(res, 404, 'incorrect email or password');
+
+        const { user_id: id, first_name: firstName, last_name: lastName } = existingUser;
+
+        const token = generateToken(id, email);
+        return res.status(200).json({
+            status: 'success',
+            message: `welcome back, ${firstName}!`,
+            data: {
+                token,
+                id,
+                firstName,
+                lastName,
+                email
+            }
+        });
+    } catch (error) {
+        return errorMessage(res, 500, 'oops! something went wrong');
+    }
+};
