cd3c4e4bff9931cddccb53f404483891948586a1
Osahon Oboite <osahonoboite@gmail.com>
2019-06-19 11:02:15 +0100

    feat(user): create api endpoint to reset password [delivers #166718262]

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,9 @@
 import regenratorRuntime from 'regenerator-runtime';
 import bcrypt from 'bcrypt';
 import { userQueries } from '../models/db/queries';
-import { generateToken } from '../middleware/jwtAuth';
+import { generateToken, getUserFromToken } from '../middleware/jwtAuth';
+import mailgun from '../config/mailgun.config';
+import generateMessageData from '../helpers/messageData';
 import errorMessage from '../helpers/responseMessages';
 
 export const signupUser = async (req, res) => {
@@ -60,6 +62,44 @@ export const loginUser = async (req, res) => {
                 lastName,
                 email
             }
+        });
+    } catch (error) {
+        return errorMessage(res, 500, 'oops! something went wrong');
+    }
+};
+
+
+export const sendResetPasswordLink = async (req, res) => {
+    try {
+        const user = await userQueries
+            .findUserByEmail(req.body.email.trim());
+
+        if (!user) return errorMessage(res, 404, 'user account not found');
+        const token = generateToken(user.user_id, user.email);
+        const resetLink = `${process.env.APP_URL}/auth/reset-password/${token}`;
+
+        const messageData = generateMessageData(user, resetLink);
+        return mailgun.messages().send(messageData, () => (
+            res.status(200).json({
+                status: 'success',
+                message: 'a password-reset link has been sent to your email'
+            })
+        ));
+    } catch (error) {
+        return errorMessage(res, 500, 'oops! something went wrong');
+    }
+};
+
+export const resetPassword = async (req, res) => {
+    const { password } = req.body;
+    try {
+        const { user_id: userId } = await getUserFromToken(req.params.token);
+        if (!userId) return errorMessage(res, 404, 'user not found. reset link may have expired');
+        const passwordHash = await bcrypt.hash(password, 10);
+        await userQueries.updatePassword(userId, passwordHash);
+        return res.status(200).json({
+            status: 'success',
+            message: 'password was successfully updated'
         });
     } catch (error) {
         return errorMessage(res, 500, 'oops! something went wrong');
